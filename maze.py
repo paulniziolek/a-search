3a4ba4d68313cbcab666456a013fb7766e936010
Paul Niziolek <niziolek.paul@gmail.com>
2023-02-03 03:16:56 -0500

    fixed fatal maze representation error

--- maze.py
+++ maze.py
@@ -10,29 +10,36 @@ class Cell():
         self.walls={'N': True, 'S': True, 'W': True, 'E': True}
         if not isWall: 
             for key in self.walls.keys(): self.walls[key]=False
-        
-    def __str__(self):
-        return f"Wall located at ({self.x}, {self.y})" if self.isWall else f"Open Space located at ({self.x}, {self.y})"
     
     def __repr__(self):
         return f"({self.x}, {self.y})"
     
     def get_pos(self) -> tuple:
         return (self.x, self.y)
+    
+    def get_neighbors(self) -> list[tuple]:
+        return [(self.x+dx, self.y+dy) for dy, dx in [(0,-1), (0,1), (-1, 0), (1, 0)]]
 
 
 class MazeArray():
     """
     Object representing Maze array of Cell() objects
     """
-    def __init__(self, n: int, m: int):
+    def __init__(self, m: int, n: int):
         self.n = n
         self.m = m
+        self.totalnodes = (n+1)*(m+1)
         self.world = []
-        for i in range(n):
+        ctr = 0
+        for i in range(m):
             self.world.append(list())
-            for j in range(m):
+            for j in range(n):
                 self.world[i].append(Cell(i, j))
+                ctr+=1
+        print(f"{ctr} nodes created")
+
+    def cell_at(self, n: tuple) -> Cell:
+        return self.world[n[1]][n[0]]
     
     def remove_wall(self, currCell: Cell, otherCell: Cell) -> None:
         if abs((currCell.x-otherCell.x)+(currCell.y-otherCell.y)) != 1:
@@ -46,24 +53,23 @@ class MazeArray():
             otherCell.walls['E'] = False
         elif otherCell.y > currCell.y:
             currCell.walls['N'] = False
-            otherCell.walls['S']
+            otherCell.walls['S'] = False
         elif otherCell.y < currCell.y:
             currCell.walls['S'] = False
-            otherCell.walls['N']
+            otherCell.walls['N'] = False
     
     def __repr__(self):
         return str(self.world)
     
     def __str__(self):
         mystr = "+--"*(self.n) + '+\n'
-
         for i in range(self.n):
             horstr = "+"
             verstr = "|"
             for j in range(self.m):
-                if self.world[i][j].walls['E']: verstr += "  |"
+                if self.world[i][j].walls['N']: verstr += "  |"
                 else: verstr += "   "
-                if self.world[i][j].walls['N']: horstr += "--+"
+                if self.world[i][j].walls['E']: horstr += "--+"
                 else: horstr += "  +"
             mystr+=verstr + '\n' + horstr + '\n'
  
@@ -74,5 +80,4 @@ def read_maze(file):
 
 def write_maze(maze, file):
     pass
-
 
