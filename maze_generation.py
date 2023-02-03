df759cd07190021028242f84eada5397469a99bc
Paul Niziolek <niziolek.paul@gmail.com>
2023-02-03 01:46:50 -0500

    dfs (failed maze gen)

--- maze_generation.py
+++ maze_generation.py
@@ -2,7 +2,7 @@ from maze import MazeArray, Cell
 from collections import deque
 import random
 
-def create_maze(m=20, n=20, isRandom=False):
+def create_maze(m=8, n=8, isRandom=False):
     if m < 1 or n < 1:
         raise Exception(f"Invalid Maze Size: {m}x{n}")
 
@@ -14,37 +14,90 @@ def create_maze(m=20, n=20, isRandom=False):
     
 # iterative implementation of DFS
 def dfs(maze, *args):
+    # initializing variables
+    m, n = maze.m, maze.n
+    visited: set(Cell) = set()
 
     # SUB METHODS START ------------------------------------------------------------------------
-    m, n = maze.m, maze.n
     # checks if starting DFS position has been provided, else, provides random position pos
-    def check_args(args: list[int]) -> tuple: 
+    def check_args(args: list[int]) -> Cell: 
         if not args:
             pos = (random.randrange(m), random.randrange(n))
         elif len(args)==2:
             pos = (args[0], args[1])
         else:
             raise Exception(f"{len(args)} arguments given but only 0 or 2 accepted.")
-        return pos
+        return maze.world[pos[0]][pos[1]]
 
     # a node is valid when it is inside the grid and hasn't been visited
-    def is_valid(node: Cell) -> bool:
-        return 0 <= node.x < n and 0 <= node.y < m and node.get_pos() not in visited
-    # SUB METHODS END ---------------------------------------------------------------------------
-    pos = check_args(args)
-    visited = set(pos)
-    print(is_valid())
-    stack = [pos]
-    while stack:
-        cell = stack.pop()
+    def is_valid(node: Cell | tuple) -> bool:
+        if isinstance(node, Cell):
+            return 0 <= node.x < n and 0 <= node.y < m and node not in visited
+        elif isinstance(node, tuple):
+            return 0 <= node[0] < n and 0 <= node[1] < m and maze.world[node[0]][node[1]] not in visited
 
-
-
+    # fills neighbors with valid maze Cells
+    def get_valid_neighbors(cell: Cell) -> list[Cell]:
+        invalidated_neighbors = cell.get_neighbors()
+        for i in range(len(invalidated_neighbors)): # technically equal to range(4)
+            if is_valid(invalidated_neighbors[i]):
+                invalidated_neighbors[i] = maze.world[invalidated_neighbors[i][0]][invalidated_neighbors[i][1]]
+        return list(filter(lambda x: isinstance(x, Cell), invalidated_neighbors)) # returns the list of validated neighbors
         
 
+    # SUB METHODS END ---------------------------------------------------------------------------
 
+    # adding in start position
+    start = check_args(args)
+    visited.add(start)
+    stack: list[Cell] = []
+    previousCell= None
+    cell = None
+
+    # TESTING
+    path = []
+
+    while stack:
+
+        # until we get a valid cell.
+        while cell not in visited and stack:
+            cell: Cell = stack.pop()
+        cell = cell.cell_at((cell.x,cell.y))
+        visited.add(cell)
+
+        # TESTING PURPOSES
+        path.append(cell)
+        print(path)
+        
+        neighbors = get_valid_neighbors(cell)
+        random.shuffle(neighbors) # shuffling order of neighbors for more random DFS
+
+        nextCell = neighbors[-1]
+        maze.remove_wall(cell, nextCell)
+        stack.append(nextCell)
+
+        
+"""
+        # logic here
+        if len(neighbors)==1:
+            maze.remove_wall(cell, neighbors[-1])
+        elif len(neighbors)>1:
+            for i in range(len(neighbors[:-1])):
+                remove = random.getrandbits(1)
+                remove = 1
+                if remove==1:
+                    maze.remove_wall(cell, neighbors[i])
+            maze.remove_wall(cell, neighbors[-1])
+        else:
+            print(cell)
+        #print(neighbors)
+        stack.extend([neighbors[-1]])
+        for i in neighbors:
+            visited.add(i)
+        print(maze)
+"""
 
 myworld = create_maze()
 print(myworld)
-
-
+dfs(myworld)
+print(myworld)
