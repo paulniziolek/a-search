dee25479ea5a6dc6cc23c151a9d3c9e119a5b046
Paul Niziolek <niziolek.paul@gmail.com>
2023-02-02 22:35:30 -0500

    polished class objects and terminal representation of maze

--- maze.py
+++ maze.py
@@ -1,27 +1,31 @@
 #myfile = open("matrix.txt", "w")
 
-
 class Cell():
     """
     Object representing a cell with walls in its compass directons
 
     """
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int, isWall:bool=True):
         self.x, self.y = x, y
-        self.isWall = True
-
+        self.walls={'N': True, 'S': True, 'W': True, 'E': True}
+        if not isWall: 
+            for key in self.walls.keys(): self.walls[key]=False
+        
     def __str__(self):
         return f"Wall located at ({self.x}, {self.y})" if self.isWall else f"Open Space located at ({self.x}, {self.y})"
     
     def __repr__(self):
         return f"({self.x}, {self.y})"
+    
+    def get_pos(self) -> tuple:
+        return (self.x, self.y)
 
 
 class MazeArray():
     """
     Object representing Maze array of Cell() objects
     """
-    def __init__(self, n, m):
+    def __init__(self, n: int, m: int):
         self.n = n
         self.m = m
         self.world = []
@@ -30,13 +34,45 @@ class MazeArray():
             for j in range(m):
                 self.world[i].append(Cell(i, j))
     
+    def remove_wall(self, currCell: Cell, otherCell: Cell) -> None:
+        if abs((currCell.x-otherCell.x)+(currCell.y-otherCell.y)) != 1:
+            raise Exception("{otherCell} not adjacent to self {currCell}")
+
+        if otherCell.x > currCell.x:
+            currCell.walls['E'] = False
+            otherCell.walls['W'] = False
+        elif otherCell.x < currCell.x:
+            currCell.walls['W'] = False
+            otherCell.walls['E'] = False
+        elif otherCell.y > currCell.y:
+            currCell.walls['N'] = False
+            otherCell.walls['S']
+        elif otherCell.y < currCell.y:
+            currCell.walls['S'] = False
+            otherCell.walls['N']
+    
     def __repr__(self):
         return str(self.world)
     
     def __str__(self):
-        mystr = ""
-        for i in range(self.m):
-            for j in range(self.n):
-                mystr += '■' if self.world[i][j].isWall else '_'
-            mystr += '\n'
+        mystr = "+--"*(self.n) + '+\n'
+
+        for i in range(self.n):
+            horstr = "+"
+            verstr = "|"
+            for j in range(self.m):
+                if self.world[i][j].walls['E']: verstr += "  |"
+                else: verstr += "   "
+                if self.world[i][j].walls['N']: horstr += "--+"
+                else: horstr += "  +"
+            mystr+=verstr + '\n' + horstr + '\n'
+ 
         return mystr
+
+def read_maze(file):
+    pass
+
+def write_maze(maze, file):
+    pass
+
+
--- maze_generation.py
+++ maze_generation.py
@@ -1,25 +1,50 @@
-from maze import MazeArray
+from maze import MazeArray, Cell
 from collections import deque
 import random
 
 def create_maze(m=20, n=20, isRandom=False):
+    if m < 1 or n < 1:
+        raise Exception(f"Invalid Maze Size: {m}x{n}")
+
     if isRandom:
-        m = random.randrange(m+1)
-        n = random.randrange(n+1)
+        m = random.randrange(m)
+        n = random.randrange(n)
+        
     return MazeArray(m, n)
-
-
-
-def dfs(maze):
     
+# iterative implementation of DFS
+def dfs(maze, *args):
 
-    pass
-    
-    
+    # SUB METHODS START ------------------------------------------------------------------------
+    m, n = maze.m, maze.n
+    # checks if starting DFS position has been provided, else, provides random position pos
+    def check_args(args: list[int]) -> tuple: 
+        if not args:
+            pos = (random.randrange(m), random.randrange(n))
+        elif len(args)==2:
+            pos = (args[0], args[1])
+        else:
+            raise Exception(f"{len(args)} arguments given but only 0 or 2 accepted.")
+        return pos
+
+    # a node is valid when it is inside the grid and hasn't been visited
+    def is_valid(node: Cell) -> bool:
+        return 0 <= node.x < n and 0 <= node.y < m and node.get_pos() not in visited
+    # SUB METHODS END ---------------------------------------------------------------------------
+    pos = check_args(args)
+    visited = set(pos)
+    print(is_valid())
+    stack = [pos]
+    while stack:
+        cell = stack.pop()
+
+
+
+        
+
+
 
 myworld = create_maze()
 print(myworld)
-
-
 
 
