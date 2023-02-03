f6983e82e06f817710875a3fd9160960c7beade2
Paul Niziolek <niziolek.paul@gmail.com>
2023-02-02 19:10:29 -0500

    create_maze() func

--- maze_generation.py
+++ maze_generation.py
@@ -1,4 +1,25 @@
 from maze import MazeArray
+from collections import deque
+import random
 
-myworld = MazeArray(20,20)
+def create_maze(m=20, n=20, isRandom=False):
+    if isRandom:
+        m = random.randrange(m+1)
+        n = random.randrange(n+1)
+    return MazeArray(m, n)
+
+
+
+def dfs(maze):
+    
+
+    pass
+    
+    
+
+myworld = create_maze()
 print(myworld)
+
+
+
+
