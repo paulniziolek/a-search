f54f232bad16b658584b636a55b3d7a681204c67
Paul Niziolek <niziolek.paul@gmail.com>
2023-02-04 18:34:52 -0500

    tested a* in main.py, results=it works!

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ from maze_generation import create_maze, dfs, create_position
 from a_star_search import a_star
 from maze import MazeArray, Cell
 
+mymaze = create_maze(25, 25)
+dfs(mymaze)
+print(mymaze)
+start = create_position(mymaze)
+end = create_position(mymaze)
 
+start = mymaze.cell_at((0, 0))
+end = mymaze.cell_at((24, 24))
+
+print(f"PATH FROM {start} TO {end} IS: ")
+mypath = a_star(mymaze, start, end)
+print(mymaze)
+print(mypath)
+print(len(mypath))
